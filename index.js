1a1dc346b6d5ea68150332423671267f2bef90b8
Grammeri <reacttsxgrammer@gmail.com>
2021-11-30 12:58:29 -0600

    Ajax_3_GET_request

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-console.log(0)
-
-$.ajax('https://repetitora.net/api/JS/Images',{
-    success: function(data){
-        data.forEach(el =>{
-            const img = document.createElement("img");
-            img.src=el.thumbnail;
-            document.querySelector("body").appendChild(img);
-        })
-    }
-});
-
-console.log(1);
-
-
-
-
-
-
-
-
-/*
 const resultBlock = document.querySelector("#result");
+const pageNumberEl = document.querySelector("#page-number");
 const clickMeButton = document.querySelector("#click-me");
 clickMeButton.addEventListener("click", makeRequest);
 
 function makeRequest(){
-    resultBlock.innerHTML = "result";
-}*/
+    $.ajax(`https://repetitora.net/api/JS/Images?page=${pageNumberEl.value}&count=1`,{
+        success: function(data){
+            data.forEach(el =>{
+                const img = document.createElement("img");
+                img.src=el.thumbnail;
+                document.querySelector("#result").appendChild(img);
+            })
+        }
+    });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
